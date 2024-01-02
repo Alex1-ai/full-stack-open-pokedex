1e41fd2cb4537c54203c8bbba86d9810210c6432
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-01-02 11:22:32 +0000

    adding the config files

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    // setupNodeEvents(on, config) {
+    setupNodeEvents(_, _) {
       // implement node event listeners here
+    
     },
   },
-});
+})
