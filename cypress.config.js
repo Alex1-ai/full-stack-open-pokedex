cdcadd640de7a225bc03c94e572035292a04472a
Alex1.ai <alexanderemmanuel1719@gmail.com>
2024-01-02 11:27:06 +0000

    disabling the eslint

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // setupNodeEvents(on, config) {
-    setupNodeEvents(_, _) {
+       /* eslint-disable */
+    setupNodeEvents(on,config) {
       // implement node event listeners here
     
     },
